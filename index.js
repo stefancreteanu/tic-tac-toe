15665224c91e1a2230e486f5dca57ac16fb67442
stefancreteanu <65374421+stefancreteanu@users.noreply.github.com>
2022-12-21 01:41:39 +0200

    update logic

--- index.js
+++ index.js
@@ -8,12 +8,35 @@ let sq7 = document.getElementById('sq7');
 let sq8 = document.getElementById('sq8');
 let sq9 = document.getElementById('sq9');
 
+const computer = 'O';
+const arr = ['sq1', 'sq2', 'sq3', 'sq4', 'sq5', 'sq6', 'sq7', 'sq8', 'sq9'];
+
 sq1.onclick = () => {
-    sq1.innerHTML = 'X'
+    sq1.innerHTML = 'X';
+
+    const index = arr.indexOf('sq1');
+
+    if(index > -1) {
+        arr.splice(index, 1);
+    }
+
+    console.log(arr);
+
+    // computerChoice = Math.floor(Math.random() * arr.length);
+
+    // console.log(arr[computerChoice]);
 }
 
 sq2.onclick = () => {
     sq2.innerHTML = 'X'
+
+    const index = arr.indexOf('sq2');
+
+    if(index > -1) {
+        arr.splice(index, 1);
+    }
+
+    console.log(arr);
 }
 
 sq3.onclick = () => {
