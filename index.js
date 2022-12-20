8e329101ee9828c6252dd3277432f0f602c6739f
stefancreteanu <65374421+stefancreteanu@users.noreply.github.com>
2022-12-20 22:36:46 +0200

    main design and logic start complete

---
+++ index.js
@@ -0,0 +1,45 @@
+let sq1 = document.getElementById('sq1');
+let sq2 = document.getElementById('sq2');
+let sq3 = document.getElementById('sq3');
+let sq4 = document.getElementById('sq4');
+let sq5 = document.getElementById('sq5');
+let sq6 = document.getElementById('sq6');
+let sq7 = document.getElementById('sq7');
+let sq8 = document.getElementById('sq8');
+let sq9 = document.getElementById('sq9');
+
+sq1.onclick = () => {
+    sq1.innerHTML = 'X'
+}
+
+sq2.onclick = () => {
+    sq2.innerHTML = 'X'
+}
+
+sq3.onclick = () => {
+    sq3.innerHTML = 'X'
+}
+
+sq4.onclick = () => {
+    sq4.innerHTML = 'X'
+}
+
+sq5.onclick = () => {
+    sq5.innerHTML = 'X'
+}
+
+sq6.onclick = () => {
+    sq6.innerHTML = 'X'
+}
+
+sq7.onclick = () => {
+    sq7.innerHTML = 'X'
+}
+
+sq8.onclick = () => {
+    sq8.innerHTML = 'X'
+}
+
+sq9.onclick = () => {
+    sq9.innerHTML = 'X'
+}
